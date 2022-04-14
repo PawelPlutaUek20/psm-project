16ae1289c60b1d659d8f664eecccecad55114cad
pawelplutauek20 <pawelpluta58@gmail.com>
2022-04-14 21:09:53 +0200

    basic login

---
+++ pages/login.tsx
@@ -0,0 +1,75 @@
+import React from "react";
+import Head from "next/head";
+
+import firebase from "firebase/app";
+import { Button, Col, Container, Row } from "react-bootstrap";
+import { withAuthUser, AuthAction } from "next-firebase-auth";
+
+const Auth = () => {
+  const [form, setForm] = React.useState({
+    email: "",
+    password: "",
+  });
+
+  const updateForm = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setForm((form) => ({ ...form, [e.target.name]: e.target.value }));
+  };
+
+  const login = () =>
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(form.email, form.password)
+      .catch((e) => console.log(e));
+
+  return (
+    <Container className="md-container d-flex align-items-center justify-content-center vh-100">
+      <Head>
+        <title>ReactJS with react-bootstrap</title>
+        <link rel="icon" href="/favicon-32x32.png" />
+      </Head>
+
+      <Container>
+        <Row className="justify-content-center">
+          <Col md="6">
+            <form>
+              <p className="h4 text-center mb-4">Sign in</p>
+              <label htmlFor="defaultFormLoginEmailEx" className="grey-text">
+                Your email
+              </label>
+              <input
+                name="email"
+                type="email"
+                onChange={updateForm}
+                id="defaultFormLoginEmailEx"
+                className="form-control"
+              />
+              <br />
+              <label htmlFor="defaultFormLoginPasswordEx" className="grey-text">
+                Your password
+              </label>
+              <input
+                name="password"
+                onChange={updateForm}
+                type="password"
+                id="defaultFormLoginPasswordEx"
+                className="form-control"
+                autoComplete="on"
+              />
+              <div className="text-center mt-4">
+                <Button color="indigo" onClick={login}>
+                  Login
+                </Button>
+              </div>
+            </form>
+          </Col>
+        </Row>
+      </Container>
+    </Container>
+  );
+};
+
+export default withAuthUser({
+  whenAuthed: AuthAction.REDIRECT_TO_APP,
+  whenUnauthedBeforeInit: AuthAction.RETURN_NULL,
+  whenUnauthedAfterInit: AuthAction.RENDER,
+})(Auth);
---
+++ utils/initAuth.ts
@@ -0,0 +1,56 @@
+import { init } from "next-firebase-auth";
+
+const TWELVE_DAYS_IN_MS = 12 * 60 * 60 * 24 * 1000;
+
+const initAuth = () => {
+  init({
+    debug: true,
+
+    // This demonstrates setting a dynamic destination URL when
+    // redirecting from app pages. Alternatively, you can simply
+    // specify `authPageURL: '/auth-ssr'`.
+    authPageURL: "/login",
+
+    // This demonstrates setting a dynamic destination URL when
+    // redirecting from auth pages. Alternatively, you can simply
+    // specify `appPageURL: '/'`.
+    appPageURL: "/",
+    loginAPIEndpoint: "/api/login",
+    logoutAPIEndpoint: "/api/logout",
+    firebaseAdminInitConfig: {
+      credential: {
+        projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID as string,
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL as string,
+        // Using JSON to handle newline problems when storing the
+        // key as a secret in Vercel. See:
+        // https://github.com/vercel/vercel/issues/749#issuecomment-707515089
+        privateKey: process.env.FIREBASE_PRIVATE_KEY
+          ? JSON.parse(process.env.FIREBASE_PRIVATE_KEY)
+          : undefined,
+      },
+      databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL as string,
+    },
+    firebaseClientInitConfig: {
+      apiKey: process.env.NEXT_PUBLIC_FIREBASE_PUBLIC_API_KEY as string,
+      authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+      databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+      projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    },
+    cookies: {
+      name: "ExampleApp",
+      keys: [
+        process.env.COOKIE_SECRET_CURRENT,
+        process.env.COOKIE_SECRET_PREVIOUS,
+      ],
+      httpOnly: true,
+      maxAge: TWELVE_DAYS_IN_MS,
+      overwrite: true,
+      path: "/",
+      sameSite: "lax",
+      secure: process.env.NEXT_PUBLIC_COOKIE_SECURE === "true",
+      signed: true,
+    },
+  });
+};
+
+export default initAuth;
