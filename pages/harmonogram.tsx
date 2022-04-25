c2602705fa3dcc41b5fca24f4564dd8c85ce39be
kudseb <kudseb42@gmail.com>
2022-04-25 15:19:09 +0200

    colour picker

--- pages/harmonogram.tsx
+++ pages/harmonogram.tsx
@@ -5,12 +5,11 @@ import {
   AuthAction,
   useAuthUser,
   withAuthUser,
-  withAuthUserTokenSSR,
 } from "next-firebase-auth";
 
 import { withNotifications } from "../components/withNotifications";
 import { GeolocationContext } from "../components/GeolocationProvider";
-import { Textarea, TextInput, Button } from "@mantine/core";
+import { Textarea, TextInput, Button, ColorInput } from "@mantine/core";
 import { Todo } from "../types";
 import { useCollection } from "@nandorojo/swr-firestore";
 
@@ -30,18 +29,9 @@ const Harmonogram = React.memo(( ) => {
     title: "",
     content: "",
     status:"idk",
-    colour:""
+    colour:"#e64980"
   })
 
-  const addtask=() =>
-    add({
-      userId: user.id!,
-      title: todo.title,
-      geolocation: geolocation,
-      content: todo.content,
-      status:"dik",
-      colour: todo.colour
-    })
   return (
   <>
   <TextInput
@@ -59,7 +49,20 @@ const Harmonogram = React.memo(( ) => {
       value={todo.content}
       onChange={(event)=>setTodo({...todo,content:event.currentTarget.value})}
     />
-    <Button fullWidth onClick={async()=> await addtask()}>add task</Button>
+    <ColorInput
+      format="hex"
+      value={todo.colour}
+      swatches={['#25262b', '#868e96', '#fa5252', '#e64980', '#be4bdb', '#7950f2', '#4c6ef5', '#228be6', '#15aabf', '#12b886', '#40c057', '#82c91e', '#fab005', '#fd7e14']}
+      onChange={(color)=>{setTodo({...todo,colour:color})}}
+    />
+    <Button fullWidth onClick={async()=> await  add({
+      userId: user.id!,
+      title: todo.title,
+      geolocation: geolocation,
+      content: todo.content,
+      status:"dik",
+      colour: todo.colour
+    })}>add task</Button>
     <ol>
         {data?.map((todo, index) => (
           <li key={index}><h5>{todo.title}</h5>{todo.content}</li>
