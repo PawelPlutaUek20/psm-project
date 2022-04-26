2bafdd73f39a9549f6431b91a70e0490e49fad18
viktoriia-kravchuk <vikakravchuk2@gmail.com>
2022-04-26 12:05:31 +0200

    main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,6 +9,8 @@ import { withNotifications } from "../components/withNotifications";
 import { GeolocationContext } from "../components/GeolocationProvider";
 import { Todo } from "../types";
 
+import { Text, Grid, SimpleGrid, Badge, Checkbox } from "@mantine/core";
+
 const Home: React.FC = () => {
   const user = useAuthUser();
   const { geolocation, setGeolocation } = React.useContext(GeolocationContext);
@@ -16,43 +18,110 @@ const Home: React.FC = () => {
   const { data, add } = useCollection<Todo>(user.id ? "todos" : null, {
     where: ["userId", "==", user.id],
   });
+
+  const task = data?.length === 1 ? "task" :"tasks";
+
+  const todos = data?.map(todo=> {
+    <tr><td>{todo.title}</td></tr>
+  });
+  
+
+  <>
+  <button onClick={() => alert(JSON.stringify(geolocation))}>
+    my location
+  </button>
+  <button onClick={() => user.signOut()}>sign out</button>
+  <Link href="/map" passHref>
+    <a>
+      <button>Map</button>
+    </a>
+  </Link>
+
+  <button
+    onClick={() =>
+      setGeolocation({
+        latitude: 50.0686,
+        longitude: 19.9551,
+      })
+    }
+  >
+    get notified
+  </button>
+  <ol>
+    {data?.map((todo, key) => (
+      <li key={key}>{todo.title}</li>
+    ))}
+  </ol>
+  
+</>
   return (
-    <>
-      <button onClick={() => alert(JSON.stringify(geolocation))}>
-        my location
-      </button>
-      <button onClick={() => user.signOut()}>sign out</button>
-      <Link href="/map" passHref>
-        <a>
-          <button>Map</button>
-        </a>
-      </Link>
-      <button
-        onClick={async () =>
-          await add({
-            title: (Math.random() + 1).toString(36).substring(7),
-            userId: user.id!,
-          })
-        }
-      >
-        add random todo
-      </button>
-      <button
-        onClick={() =>
-          setGeolocation({
-            latitude: 50.0686,
-            longitude: 19.9551,
-          })
-        }
-      >
-        get notified
-      </button>
-      <ol>
-        {data?.map((todo, key) => (
-          <li key={key}>{todo.title}</li>
-        ))}
-      </ol>
-    </>
+    <Grid>
+      <Grid.Col 
+      span={12}
+    style={{
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      marginTop:30,
+      marginBottom: 10
+    }}>
+      <Text weight={600} size="xl">You have {data?.length} {task} this month</Text>
+ </Grid.Col>
+ <Grid.Col 
+      span={12}
+    style={{
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      marginBottom: 10
+    }}>
+   <input type="search" placeholder="Search for tasks.." style={{
+     backgroundImage: "url(https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Vector_search_icon.svg/800px-Vector_search_icon.svg.png",
+     backgroundSize: "15px 15px",
+     backgroundPosition: "10px 12px",
+     backgroundRepeat: "no-repeat",
+     width:"80%",
+     padding: "12px 20px 12px 40px",
+     border: "5px solid #ddd",
+   }}/>
+   
+ </Grid.Col>
+ <Grid.Col 
+      span={12}
+    style={{
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      marginBottom: 10,
+    }}>
+
+    <SimpleGrid cols={3} spacing="lg">
+      <div><Checkbox/>TO-DO</div>
+      <div><Checkbox/>Progress</div>
+      <div><Checkbox/>Done</div>
+    </SimpleGrid>
+
+ </Grid.Col>
+ <Grid.Col 
+      span={12}
+    style={{
+      display: "flex",
+      justifyContent: "left",
+      alignItems: "left",
+      marginLeft:"10%",
+      marginTop:30,
+      marginBottom: 10
+    }}>
+      <Text weight={600} size="xl">Today's Tasks</Text>
+ </Grid.Col>
+ <Grid.Col>
+   <table>
+
+   </table>
+   
+ </Grid.Col>
+    </Grid>
+
   );
 };
 
